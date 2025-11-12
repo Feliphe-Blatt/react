6da1b3a6a3c02cd45a1ae7a089a0e506c8724079
feliphe blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2025-11-12 20:31:06 -0300

    Exercise: adição de json server, axios e preparando para o BootStrap.

--- aula06/src/pages/DpoLgpd.jsx
+++ aula06/src/pages/DpoLgpd.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const DpoLgpd = () => {
+  
+  const [dpo, setdpo] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/dpolgpd")
+      .then(response => {
+        setdpo(response.data)
+      })
+      .catch(error => {
+        console.error("Deu erro ao buscar os dados do DPO:", error)
+      })
+  }, [])
+
   return (
     <div>
         <main>
-            <h1>Política de Privacidade</h1>
-            <p>Esta é a página da política de privacidade do nosso site.</p>
+            <h1>DPO LGPD</h1>
+            <p>Esta é a página do DPO (Data Protection Officer) da nossa aplicação.</p>
+            <ul>
+              {dpo.map(dpo => (
+                <li key={dpo.id}>
+                  {dpo.texto}
+                </li>
+              ))}
+            </ul>
         </main>
     </div>
   )
